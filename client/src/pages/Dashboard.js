5e9a9b05826abcf1724df5b9a9c20814ce75d067
kidushailu <hailu.kidus1@gmail.com>
2025-03-23 22:00:50 -0400

    pulls repository data from backend and displays it. also added a button linked to the full repo

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,27 +1,117 @@
-import { BarChart, LineChart, XAxis, YAxis, Bar, Line } from "recharts";
-import { useState } from "react";
+import { BarChart, LineChart, XAxis, YAxis, Bar, Line, Tooltip, Legend } from "recharts";
+import { useState, useEffect } from "react";
 import './Dashboard.css';
+import { useLocation } from "react-router-dom";
 
 export default function Dashboard() {
-  const [search, setSearch] = useState("");
+  const location = useLocation();
+  const repoUrl = location.state?.repoUrl || "";
+  const [search, setSearch] = useState('');
+  const [totalCommits, setTotalCommits] = useState(0);
+  const [totalPulls, setTotalPulls] = useState(0);
+  const [contributors, setContributors] = useState([]);
+  const [filesCount, setFilesCount] = useState(0);
+  const [chartData, setChartData] = useState([
+    { month: "January", commits: 0, pullRequests: 0 },
+    { month: "February", commits: 0, pullRequests: 0 },
+    { month: "March", commits: 0, pullRequests: 0 },
+    { month: "April", commits: 0, pullRequests: 0 },
+    { month: "May", commits: 0, pullRequests: 0 },
+    { month: "June", commits: 0, pullRequests: 0 },
+    { month: "July", commits: 0, pullRequests: 0 },
+    { month: "August", commits: 0, pullRequests: 0 },
+    { month: "September", commits: 0, pullRequests: 0 },
+    { month: "October", commits: 0, pullRequests: 0 },
+    { month: "November", commits: 0, pullRequests: 0 },
+    { month: "December", commits: 0, pullRequests: 0 },
+  ]);
 
-  const data = [
-    { month: "January", commits: 50, pullRequests: 40 },
-    { month: "February", commits: 60, pullRequests: 50 },
-    { month: "March", commits: 80, pullRequests: 70 },
-    { month: "April", commits: 90, pullRequests: 80 },
-    { month: "May", commits: 70, pullRequests: 75 },
-    { month: "June", commits: 60, pullRequests: 65 },
-    { month: "July", commits: 50, pullRequests: 45 },
-  ];
+  const getCommits = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/searchRepo/commits?repoUrl=${encodeURIComponent(repoUrl)}`);
 
+      if (!response.ok) {console.error('Error fetching repositories:', response.statusText);}
+
+      const data = await response.json();
+      setTotalCommits(data.total);
+      
+      for (const item of data.commits) {
+        const date = new Date(item.commit.committer.date);
+        const month = date.getUTCMonth()
+        chartData[month].commits++;
+      };
+      setChartData([...chartData]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  const getPulls = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/searchRepo/pulls?repoUrl=${encodeURIComponent(repoUrl)}`);
+      if (!response.ok) console.error('Error fetching repositories:', response.statusText);
+      const data = await response.json();
+      setTotalPulls(data.length);
+
+      for (const item of data) {
+        const date = new Date(item.created_at);
+        const month = date.getUTCMonth()
+        chartData[month].pullRequests++;
+      };
+      setChartData([...chartData]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  const getContributors = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/searchRepo/contributors?repoUrl=${encodeURIComponent(repoUrl)}`);
+      if (response.ok) {
+        const data = await response.json();
+        setContributors(data);
+      } else {
+        console.error('Error fetching repositories:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  const getFilesCount = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/searchRepo/files?repoUrl=${encodeURIComponent(repoUrl)}`);
+      if (response.ok) {
+        const data = await response.json();
+        setFilesCount(data);
+      } else {
+        console.error('Error fetching repositories:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    if (repoUrl) {
+    getCommits();
+    getPulls();
+    getContributors();
+    getFilesCount();
+    }
+  }, [repoUrl]);
+
+  
   return (
     <div className="container">
       <header className="flex justify-between items-center pb-4">
         <h1 className="title">Dashboard</h1>
-        <button className="search-button" variant="outline">View Repository</button>
+        {repoUrl && (
+          <a href={repoUrl} target="_blank" rel="noopener noreferrer" className="button-link">
+            View Repository
+          </a>
+        )}      
       </header>
-
       <div class="container text-center">
         <div class="row align-items-start">
           <div class="col">
@@ -30,17 +120,17 @@ export default function Dashboard() {
           <div class="col">
             <div className="dashbox metric-row">
               <h2>Metrics</h2>
-              <div className="commits">Commits: <strong>873</strong></div>
-              <div className="num-files">Total Files: <strong>345</strong></div>
-              <div className="pull-requests">Pull Requests: <strong>78</strong></div>
+              <div className="commits">Commits: <strong>{totalCommits}</strong></div>
+              <div className="num-files">Total Files: <strong>{filesCount}</strong></div>
+              <div className="pull-requests">Pull Requests: <strong>{totalPulls}</strong></div>
               <div className="top-contributors">
                 Top Contributors:
                 <strong>
-                  <ul>
-                    <li>Mark</li>
-                    <li>Jeff</li>
-                    <li>Sarah</li>
-                  </ul>
+                  <ol>
+                    {contributors.map((contributor) => (
+                      <li key={contributor.id}>{contributor.login}</li>
+                    ))}
+                  </ol>
                 </strong>
               </div>
             </div>
@@ -50,17 +140,21 @@ export default function Dashboard() {
               <div className="charts">
                 <div className="dashbox line-chart">
                   <h2>Commit Trends</h2>
-                  <LineChart data={data} width={400} height={200}>
+                  <LineChart data={chartData} width={400} height={200}>
                     <XAxis dataKey="month" />
                     <YAxis />
+                    <Tooltip />
+                    <Legend />
                     <Line type="monotone" dataKey="commits" stroke="#8884d8" />
                   </LineChart>
                 </div>
                 <div className="dashbox bar-chart">
                   <h2>Pull Request Trends</h2>
-                  <BarChart data={data} width={400} height={200}>
+                  <BarChart data={chartData} width={400} height={200}>
                     <XAxis dataKey="month" />
                     <YAxis />
+                    <Tooltip />
+                    <Legend />
                     <Bar dataKey="pullRequests" fill="#82ca9d" />
                   </BarChart>
                 </div>
