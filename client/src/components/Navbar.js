d50137901f4071adc945882e9ccb5e61230e7857
kidushailu <hailu.kidus1@gmail.com>
2025-03-23 21:55:34 -0400

    search bar now takes in repository url and forwards it to the dashboard

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {React, useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useMatch, useResolvedPath } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-    return (
-        <nav className="nav">
-          <Link to="/" className="site-title">
-            <img width="65" height="65" src="RepoInsight-logo-pptexport.png" alt="RepoInsight logo" className="site-logo"></img>
-            <span>RepoInsight</span>
-          </Link>
-          <div className='repo-search'>
-            <input type="text" placeholder="Search for a repository" className='search-bar'></input>
-          </div>
-          <ul>
-            <CustomLink to="/dashboard">Dashboard</CustomLink>
-            <CustomLink to="/individual-insights">Individual Insights</CustomLink>
-            <CustomLink to="/line-modification">Line Modification</CustomLink>
-            <CustomLink to="/login">Login</CustomLink>
-            <CustomLink to="/sign-up">Sign Up</CustomLink>
-          </ul>
-        </nav>
-    )
+  const [repoUrl, setRepoUrl] = useState('');
+  const navigate = useNavigate();
+
+  const searchRepo = () => {
+    navigate('/dashboard', { state: { repoUrl } });
+  }
+
+  return (
+      <nav className="nav">
+        <Link to="/" className="site-title">
+          <img width="65" height="65" src="RepoInsight-logo-pptexport.png" alt="RepoInsight logo" className="site-logo"></img>
+          <span>RepoInsight</span>
+        </Link>
+        <div className='repo-search'>
+          <input type="text" value={repoUrl} placeholder="Search for a repository" className='search-bar' onChange={(e) => setRepoUrl(e.target.value)} />
+          <button onClick={searchRepo}>Search</button>
+        </div>
+        <ul>
+          <CustomLink to="/dashboard">Dashboard</CustomLink>
+          <CustomLink to="/individual-insights">Individual Insights</CustomLink>
+          <CustomLink to="/line-modification">Line Modification</CustomLink>
+          <CustomLink to="/login">Login</CustomLink>
+          <CustomLink to="/sign-up">Sign Up</CustomLink>
+        </ul>
+      </nav>
+  )
 }
 
 function CustomLink({ to, children, ...props }) {
